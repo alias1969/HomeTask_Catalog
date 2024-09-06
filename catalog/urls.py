e99995387904bbeeee0c0fedae7e327bbcf3e835
Irina Kamneva <brynza80@gmail.com>
2024-09-06 21:00:19 +0300

    fix code

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,16 +5,15 @@ from tkinter.font import names
 from xml.etree.ElementInclude import include
 
 from django.contrib import admin
-from catalog.apps import CatalogConfig
 from django.urls import path
-from catalog.views import home
-from catalog.views import contacts
+from catalog.views import home, contacts
+from catalog.apps import CatalogConfig
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', home, name = 'home'),
-    path('/contacts', contacts, name = 'contacts')
+    path('contacts/', contacts, name = 'contacts')
     ]
 
 
