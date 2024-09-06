756a97a978de269f571fe61b2e9869ffd1169b51
Irina Kamneva <brynza80@gmail.com>
2024-09-06 21:42:09 +0300

    additional task

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 
+
 def home(request):
+    """ Домашняя страница"""
     return render(request, 'home.html')
 
 
 def contacts(request):
+    """ Страница контактов """
+    # Получить от пользователя его данные для обратной связи
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f""" Данные для обратной связи:
+        Имя: {name}, 
+        Телефон: {phone}, 
+        Доп. информация: {message}
+        """)
+
     return render(request, 'contacts.html')
+
+
 
